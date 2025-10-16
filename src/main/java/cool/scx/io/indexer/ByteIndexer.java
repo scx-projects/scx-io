ea1095b2ae5733c931cda19461053a6328d2cde7
scx567888 <scx567888@outlook.com>
2025-10-16 17:44:24 +0800

    Dev (#6)

--- src/main/java/cool/scx/io/indexer/ByteIndexer.java
+++ src/main/java/cool/scx/io/indexer/ByteIndexer.java
@@ -12,6 +12,7 @@ public interface ByteIndexer {
 
     /// 支持跨 chunk 的回溯匹配, 因此返回值可能为负数
     /// 若未匹配到 请返回 NO_MATCH
+    /// 可以不处理 空匹配模式 的边界情况 (上层会根据 isEmptyPattern 直接走快速路径), 但要保证 isEmptyPattern 实现正确
     ///
     /// @param chunk chunk
     /// @return 匹配的索引位置
