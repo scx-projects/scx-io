c50254fb9dcead032e74e0e98cd08baadd7674a3
scx567888 <scx567888@outlook.com>
2025-10-20 10:52:50 +0800

    dev

--- src/main/java/cool/scx/io/ByteChunkQueue.java
+++ src/main/java/cool/scx/io/ByteChunkQueue.java
@@ -8,6 +8,13 @@ public class ByteChunkQueue {
 
     private ByteNode head;
     private ByteNode tail;
+    private long totalLength;
+
+    public ByteChunkQueue() {
+        this.head = null;
+        this.tail = null;
+        this.totalLength = 0;
+    }
 
     public void append(ByteChunk chunk) {
         var node = new ByteNode(chunk);
@@ -18,6 +25,7 @@ public class ByteChunkQueue {
             tail.next = node;
             tail = tail.next;
         }
+        this.totalLength += chunk.length;
     }
 
     public ByteChunk next() {
@@ -29,11 +37,16 @@ public class ByteChunkQueue {
         if (head == null) {
             tail = null;
         }
+        this.totalLength -= chunk.length;
         return chunk;
     }
 
     public boolean isEmpty() {
         return head == null;
+    }
+
+    public long totalLength() {
+        return totalLength;
     }
 
 }
