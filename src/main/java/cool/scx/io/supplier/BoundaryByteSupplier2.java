e57f83b32d7be4e176837517c6c5b7c6fe4ba1ce
scx567888 <scx567888@outlook.com>
2025-10-25 15:53:20 +0800

    dev

--- src/main/java/cool/scx/io/supplier/BoundaryByteSupplier2.java
+++ src/main/java/cool/scx/io/supplier/BoundaryByteSupplier2.java
@@ -9,6 +9,9 @@ import cool.scx.io.exception.NoMoreDataException;
 import cool.scx.io.exception.ScxIOException;
 import cool.scx.io.indexer.KMPByteIndexer;
 
+import static cool.scx.io.ByteChunk.EMPTY_CHUNK;
+import static cool.scx.io.indexer.ByteIndexer.NO_MATCH;
+
 
 /// BoundaryByteSupplier
 ///
@@ -21,6 +24,7 @@ public final class BoundaryByteSupplier2 implements ByteSupplier {
     private final boolean autoClose;
     private final ByteChunkByteConsumer consumer;
     private final ByteChunkQueue cache;
+    private boolean useCache;
     private boolean isFinish;
 
     public BoundaryByteSupplier2(ByteInput byteInput, byte[] boundaryBytes) {
@@ -33,19 +37,66 @@ public final class BoundaryByteSupplier2 implements ByteSupplier {
         this.autoClose = autoClose;
         this.consumer = new ByteChunkByteConsumer();
         this.cache = new ByteChunkQueue();
+        this.useCache = false;
         this.isFinish = this.byteIndexer.isEmptyPattern();
     }
 
     @Override
     public ByteChunk get() throws AlreadyClosedException, ScxIOException, NoMoreDataException {
-        //完成了就永远返回 null
+        // 如果需要使用缓存
+        if (useCache) {
+            var cacheChunk = cache.next();
+            if (cacheChunk != null) {
+                return cacheChunk;
+            }
+            // 缓存中已经没有数据
+            useCache = false;
+        }
+
+        // 完成了就永远返回 null
         if (isFinish) {
             return null;
         }
-        byteInput.read(consumer, Long.MAX_VALUE);
+
+        byteInput.peek(consumer, Long.MAX_VALUE);
         var byteChunk = consumer.byteChunk();
         var i = byteIndexer.indexOf(byteChunk);
-        return byteChunk;
+
+        System.out.println(byteChunk);
+
+        if (i != NO_MATCH) {
+            var safeLength = i + byteIndexer.pattern().length;
+            byteInput.skipFully(safeLength);
+            byteChunk = byteChunk.subChunk(0, safeLength);
+            isFinish = true;
+            if (cache.isEmpty()) {
+                return byteChunk;
+            } else {
+                cache.append(byteChunk);
+                useCache = true;
+                return EMPTY_CHUNK;
+            }
+        }
+
+        // 未匹配到, 需要判断是 完全未匹配 还是 部分匹配
+        if (byteIndexer.matchedLength() == 0) {
+            // 完全未匹配 表示当前块可以 安全使用
+            byteInput.skipFully(byteChunk.length);
+            // 如果当前缓存 没有数据直接返回 否则添加到缓存中等待下次 读取
+            if (cache.isEmpty()) {
+                return byteChunk;
+            } else {
+                cache.append(byteChunk);
+                useCache = true;
+                return EMPTY_CHUNK;
+            }
+        } else {
+            // 部分匹配 我们不能确定这个块是安全的 所以先缓存起来 等待下次读取
+            byteInput.skipFully(byteChunk.length);
+            cache.append(byteChunk);
+            return EMPTY_CHUNK;
+        }
+
     }
 
     @Override
