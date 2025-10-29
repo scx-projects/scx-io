b37c1e7f23da64c719b6524f28b509e85d55625b
scx567888 <scx567888@outlook.com>
2025-10-29 12:03:43 +0800

    dev

--- src/main/java/cool/scx/io/indexer/ByteIndexer.java
+++ src/main/java/cool/scx/io/indexer/ByteIndexer.java
@@ -4,7 +4,7 @@ import cool.scx.io.ByteChunk;
 
 /// ByteIndexer
 ///
-/// 有状态的 索引匹配器
+/// 支持跨 chunk 的 的回溯匹配, 有状态的 索引匹配器, 可连续调用 indexOf.
 ///
 /// @author scx567888
 /// @version 0.0.1
@@ -12,9 +12,11 @@ public interface ByteIndexer {
 
     int NO_MATCH = Integer.MIN_VALUE;
 
-    /// 支持跨 chunk 的回溯匹配, 因此返回值可能为负数
-    /// 若未匹配到 请返回 NO_MATCH
-    /// 可以不处理 空匹配模式 的边界情况 (上层会根据 patternLength == 0 直接走快速路径), 但要保证 patternLength 实现正确
+    ///
+    /// - 若未匹配到, 请返回 NO_MATCH.
+    /// - 若匹配到, 请返回 相对于 当前 chunk 的索引值,
+    ///   因为跨 chunk 的原因, 索引值位于之前的 chunk, 这时可以允许 负数索引,
+    /// - 可以不处理 空匹配模式 的边界情况 (上层会根据 patternLength == 0 直接走快速路径), 但要保证 patternLength 实现正确
     ///
     /// @param chunk chunk
     /// @return 匹配的索引位置
@@ -23,7 +25,7 @@ public interface ByteIndexer {
     /// 模式串长度
     int patternLength();
 
-    /// 当前已匹配的长度
+    /// 当前已经匹配的字节长度
     int matchedLength();
 
     /// 重置匹配状态
