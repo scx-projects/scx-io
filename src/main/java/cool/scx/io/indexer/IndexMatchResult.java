5c5a373ff9b63547e5861e13259352ba2a31ea79
scx567888 <scx567888@outlook.com>
2025-10-29 18:20:54 +0800

    dev

---
+++ src/main/java/cool/scx/io/indexer/IndexMatchResult.java
@@ -0,0 +1,44 @@
+package cool.scx.io.indexer;
+
+import static cool.scx.io.indexer.IndexMatchStatus.*;
+
+/// IndexMatchStatus
+///
+/// @author scx567888
+/// @version 0.0.1
+public final class IndexMatchResult {
+
+    public static final IndexMatchResult NO_MATCH_RESULT = new IndexMatchResult(NO_MATCH, -1, -1);
+
+    public static final IndexMatchResult PARTIAL_MATCH_RESULT = new IndexMatchResult(PARTIAL_MATCH, -1, -1);
+
+    /// 匹配状态
+    public final IndexMatchStatus status;
+
+    /// - 若完全未匹配或部分匹配, 该数据无意义.
+    /// - 若完全匹配, 表示 相对于 当前 chunk 的索引值, 因为允许跨 chunk, 索引值可能位于之前的 chunk, 这时可以是 负数索引.
+    public final int index;
+
+    /// - 若完全未匹配或部分匹配, 该数据无意义.
+    /// - 若完全匹配, 表示本次实际匹配的长度
+    public final int matchedLength;
+
+    private IndexMatchResult(IndexMatchStatus status, int index, int matchedLength) {
+        this.status = status;
+        this.index = index;
+        this.matchedLength = matchedLength;
+    }
+
+    public static IndexMatchResult noMatch() {
+        return NO_MATCH_RESULT;
+    }
+
+    public static IndexMatchResult partialMatch() {
+        return PARTIAL_MATCH_RESULT;
+    }
+
+    public static IndexMatchResult fullMatch(int index, int matchedLength) {
+        return new IndexMatchResult(FULL_MATCH, index, matchedLength);
+    }
+
+}
---
+++ src/main/java/cool/scx/io/indexer/IndexMatchStatus.java
@@ -0,0 +1,18 @@
+package cool.scx.io.indexer;
+
+/// IndexMatchStatus
+///
+/// @author scx567888
+/// @version 0.0.1
+public enum IndexMatchStatus {
+
+    /// 完全未匹配
+    NO_MATCH,
+
+    /// 部分匹配
+    PARTIAL_MATCH,
+
+    /// 完全匹配
+    FULL_MATCH
+
+}
