e008522b432798364ee12344f93e62b540e2b1f9
scx567888 <scx567888@outlook.com>
2025-10-29 19:06:28 +0800

    dev

--- src/main/java/cool/scx/io/indexer/LineBreakIndexer.java
+++ src/main/java/cool/scx/io/indexer/LineBreakIndexer.java
@@ -2,6 +2,8 @@ package cool.scx.io.indexer;
 
 import cool.scx.io.ByteChunk;
 
+import static cool.scx.io.indexer.IndexMatchResult.*;
+
 /// LineBreakIndexer
 ///
 /// 可同时匹配 `\r\n` 或 `\n` .
@@ -17,17 +19,17 @@ public final class LineBreakIndexer implements ByteIndexer {
     }
 
     @Override
-    public int indexOf(ByteChunk chunk) {
+    public IndexMatchResult indexOf(ByteChunk chunk) {
 
         for (int i = 0; i < chunk.length; i = i + 1) {
 
             var currentByte = chunk.getByte(i);
 
             // 未曾匹配 或者 已经处于匹配成功状态
-            if (matchedLength == 0 || matchedLength == 2) {
+            if (matchedLength == 0) {
                 if (currentByte == '\n') {
-                    matchedLength = 1; // \n 匹配成功
-                    return i;
+                    matchedLength = 0; // \n 匹配成功
+                    return fullMatch(i, 1);
                 } else if (currentByte == '\r') {
                     matchedLength = 1; // 暂存状态, 等待 \n
                 } else {
@@ -35,8 +37,8 @@ public final class LineBreakIndexer implements ByteIndexer {
                 }
             } else if (matchedLength == 1) {
                 if (currentByte == '\n') {
-                    matchedLength = 2; // \r\n 匹配成功
-                    return i - 1;
+                    matchedLength = 0; // \r\n 匹配成功
+                    return fullMatch(i - 1, 2);
                 } else if (currentByte == '\r') {
                     matchedLength = 1; // 当前字符又是 \r, 重启匹配
                 } else {
@@ -45,17 +47,12 @@ public final class LineBreakIndexer implements ByteIndexer {
             }
         }
 
-        return NO_MATCH;
+        return matchedLength == 0 ? NO_MATCH_RESULT : PARTIAL_MATCH_RESULT;
     }
 
     @Override
     public boolean isEmptyPattern() {
         return false;
-    }
-
-    @Override
-    public int matchedLength() {
-        return matchedLength;
     }
 
     @Override
