0ef320b9e92ec153eae1dd7025e8332bd1d7466c
scx567888 <scx567888@outlook.com>
2025-10-30 11:42:30 +0800

    dev

--- src/test/java/cool/scx/io/test/BoundaryByteSupplierTest.java
+++ src/test/java/cool/scx/io/test/BoundaryByteSupplierTest.java
@@ -5,6 +5,7 @@ import cool.scx.io.exception.AlreadyClosedException;
 import cool.scx.io.exception.NoMoreDataException;
 import cool.scx.io.exception.ScxIOException;
 import cool.scx.io.indexer.BitMaskByteIndexer;
+import cool.scx.io.indexer.KMPByteIndexer;
 import cool.scx.io.supplier.BoundaryByteSupplier;
 import cool.scx.io.supplier.InputStreamByteSupplier;
 import org.testng.Assert;
@@ -17,15 +18,29 @@ public class BoundaryByteSupplierTest {
 
     public static void main(String[] args) throws NoMoreDataException, AlreadyClosedException, ScxIOException {
         test1();
+        test2();
     }
 
     @Test
     public static void test1() throws NoMoreDataException, AlreadyClosedException, ScxIOException {
         var str = "1234567890888866661111aaaabhellhellhellhhhhheeeeelllbbcccdddeeefffggghhhiiihelloend enden f";
-        var rawByteReader = new DefaultByteInput(new InputStreamByteSupplier(new ByteArrayInputStream(str.getBytes(StandardCharsets.UTF_8)), 1));
-        var newByteReader = new DefaultByteInput(new BoundaryByteSupplier(rawByteReader, new BitMaskByteIndexer("hello".getBytes(StandardCharsets.UTF_8))));
-        var read = newByteReader.readAll();
-        Assert.assertEquals(new String(read), "1234567890888866661111aaaabhellhellhellhhhhheeeeelllbbcccdddeeefffggghhhiii");
+        for (int i = 1; i < 1000; i++) {
+            var rawByteReader = new DefaultByteInput(new InputStreamByteSupplier(new ByteArrayInputStream(str.getBytes(StandardCharsets.UTF_8)), i));
+            var newByteReader = new DefaultByteInput(new BoundaryByteSupplier(rawByteReader, new BitMaskByteIndexer("hello".getBytes(StandardCharsets.UTF_8))));
+            var read = newByteReader.readAll();
+            Assert.assertEquals(new String(read), "1234567890888866661111aaaabhellhellhellhhhhheeeeelllbbcccdddeeefffggghhhiii");
+        }
+    }
+
+    @Test
+    public static void test2() throws NoMoreDataException, AlreadyClosedException, ScxIOException {
+        var str = "1234567890888866661111aaaabhellhellhellhhhhheeeeelllbbcccdddeeefffggghhhiiihelloend enden f";
+        for (int i = 1; i < 1000; i++) {
+            var rawByteReader = new DefaultByteInput(new InputStreamByteSupplier(new ByteArrayInputStream(str.getBytes(StandardCharsets.UTF_8)), i));
+            var newByteReader = new DefaultByteInput(new BoundaryByteSupplier(rawByteReader, new KMPByteIndexer("hello".getBytes(StandardCharsets.UTF_8))));
+            var read = newByteReader.readAll();
+            Assert.assertEquals(new String(read), "1234567890888866661111aaaabhellhellhellhhhhheeeeelllbbcccdddeeefffggghhhiii");
+        }
     }
 
 }
