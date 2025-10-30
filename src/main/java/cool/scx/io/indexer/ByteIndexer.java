eab827e1ef32a97470d45ba0b36c7377279c2716
scx567888 <scx567888@outlook.com>
2025-10-30 10:39:11 +0800

    dev

--- src/main/java/cool/scx/io/indexer/ByteIndexer.java
+++ src/main/java/cool/scx/io/indexer/ByteIndexer.java
@@ -12,7 +12,7 @@ public interface ByteIndexer {
 
     /// - 匹配成功需重置状态, 以便能够直接进行下次匹配.
     /// - 可以不处理 空匹配模式 的边界情况 (上层会根据 patternLength == 0 直接走快速路径), 但要保证 patternLength 实现正确.
-    IndexMatchResult indexOf(ByteChunk chunk);
+    StatusIndexMatchResult indexOf(ByteChunk chunk);
 
     /// 是否为空匹配模式
     /// - 对于固定长度模式串: 返回 是否是空模式串
