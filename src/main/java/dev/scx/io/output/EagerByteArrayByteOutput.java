a08518ee41ee972b78ef59c592e9d22dc0d232cc
scx567888 <scx567888@outlook.com>
2026-01-27 17:36:35 +0800

    dev

---
+++ src/main/java/dev/scx/io/output/EagerByteArrayByteOutput.java
@@ -0,0 +1,54 @@
+package dev.scx.io.output;
+
+import dev.scx.io.ByteChunk;
+import dev.scx.io.consumer.ByteArrayByteConsumer;
+import dev.scx.io.consumer.EagerByteArrayByteConsumer;
+import dev.scx.io.exception.OutputAlreadyClosedException;
+
+import java.io.IOException;
+
+/// ByteArrayByteOutput
+///
+/// 这里直接借用 [EagerByteArrayByteConsumer] 来实现
+///
+/// @author scx567888
+/// @version 0.0.1
+public final class EagerByteArrayByteOutput extends AbstractByteOutput {
+
+    private final EagerByteArrayByteConsumer byteConsumer;
+
+    public EagerByteArrayByteOutput() {
+        this.byteConsumer = new EagerByteArrayByteConsumer();
+    }
+
+    @Override
+    public void write(byte b) throws OutputAlreadyClosedException {
+        ensureOpen();
+
+        byteConsumer.accept(ByteChunk.of(new byte[]{b}));
+    }
+
+    @Override
+    public void write(ByteChunk b) throws OutputAlreadyClosedException {
+        ensureOpen();
+
+        byteConsumer.accept(b);
+    }
+
+    @Override
+    public void flush() throws OutputAlreadyClosedException {
+        ensureOpen();
+    }
+
+    @Override
+    public void close() throws OutputAlreadyClosedException {
+        ensureOpen();
+
+        closed = true;
+    }
+
+    public byte[] bytes() {
+        return byteConsumer.bytes();
+    }
+
+}
