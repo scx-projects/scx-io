7e75c8c9dad18c059ea7a58bcd80ec09b763f878
scx567888 <scx567888@outlook.com>
2026-01-27 17:35:33 +0800

    dev

--- src/main/java/dev/scx/io/consumer/ByteArrayByteConsumer.java
+++ src/main/java/dev/scx/io/consumer/ByteArrayByteConsumer.java
@@ -2,83 +2,77 @@ package dev.scx.io.consumer;
 
 import dev.scx.io.ByteChunk;
 
-import java.util.Arrays;
-
-/// ByteArrayByteConsumer
-///
-/// 逻辑来自 [java.io.ByteArrayOutputStream].
+/// LazyByteArrayByteConsumer
 ///
 /// @author scx567888
 /// @version 0.0.1
 public final class ByteArrayByteConsumer implements ByteConsumer {
 
-    private static final int SOFT_MAX_ARRAY_LENGTH = Integer.MAX_VALUE - 8;
-
-    private byte[] bytes;
+    private ByteChunkNode head;
+    private ByteChunkNode tail;
     private int total;
 
     public ByteArrayByteConsumer() {
-        this(32);
-    }
-
-    public ByteArrayByteConsumer(int size) {
-        this.bytes = new byte[size];
+        this.head = null;
+        this.tail = null;
         this.total = 0;
     }
 
-    private static int newLength(int oldLength, int minGrowth, int prefGrowth) {
-        // preconditions not checked because of inlining
-        // assert oldLength >= 0
-        // assert minGrowth > 0
-
-        int prefLength = oldLength + Math.max(minGrowth, prefGrowth); // might overflow
-        if (0 < prefLength && prefLength <= SOFT_MAX_ARRAY_LENGTH) {
-            return prefLength;
-        } else {
-            // put code cold in a separate method
-            return hugeLength(oldLength, minGrowth);
-        }
-    }
-
-    private static int hugeLength(int oldLength, int minGrowth) {
-        int minLength = oldLength + minGrowth;
-        if (minLength < 0) { // overflow
-            throw new OutOfMemoryError("Required array length " + oldLength + " + " + minGrowth + " is too large");
-        } else if (minLength <= SOFT_MAX_ARRAY_LENGTH) {
-            return SOFT_MAX_ARRAY_LENGTH;
-        } else {
-            return minLength;
-        }
-    }
-
-    private void ensureCapacity(int minCapacity) {
-        // overflow-conscious code
-        int oldCapacity = bytes.length;
-        int minGrowth = minCapacity - oldCapacity;
-        if (minGrowth > 0) {
-            bytes = Arrays.copyOf(bytes, newLength(oldCapacity,
-                minGrowth, oldCapacity /* preferred growth */));
-        }
-    }
-
     @Override
-    public boolean accept(ByteChunk chunk) {
-        ensureCapacity(total + chunk.length);
-        System.arraycopy(chunk.bytes, chunk.start, bytes, total, chunk.length);
-        total += chunk.length;
+    public boolean accept(ByteChunk byteChunk) {
+        var dataNode = new ByteChunkNode(byteChunk);
+        if (head == null) {
+            head = dataNode;
+            tail = head;
+        } else {
+            tail.next = dataNode;
+            tail = tail.next;
+        }
+        total += byteChunk.length;
         return true;
     }
 
-    public ByteChunk chunk() {
-        return ByteChunk.of(bytes, 0, total);
-    }
-
     public byte[] bytes() {
-        return chunk().getBytes();
+        var node = head;
+
+        // 从未调用 accept 会导致此情况
+        if (node == null) {
+            return new byte[0];
+        }
+
+        // 只调用了一次 accept, 我们直接返回当前数据
+        if (node.next == null) {
+            return node.chunk.getBytes();
+        }
+
+        // 多个数据我们合并
+        var bytes = new byte[total];
+        int offset = 0;
+
+        do {
+            int chunkLength = node.chunk.length;
+            int chunkOffset = node.chunk.start;
+            System.arraycopy(node.chunk.bytes, chunkOffset, bytes, offset, chunkLength);
+            offset += chunkLength;
+            node = node.next;
+        } while (node != null);
+
+        return bytes;
     }
 
     public int size() {
         return total;
+    }
+
+    private static class ByteChunkNode {
+
+        public final ByteChunk chunk;
+        public ByteChunkNode next;
+
+        public ByteChunkNode(ByteChunk chunk) {
+            this.chunk = chunk;
+        }
+
     }
 
 }
