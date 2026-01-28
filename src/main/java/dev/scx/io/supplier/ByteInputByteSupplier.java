6665fa64a1420169aa87c29be3e8e75833a0e412
scx567888 <scx567888@outlook.com>
2026-01-28 10:59:23 +0800

    dev

--- src/main/java/dev/scx/io/supplier/ByteInputByteSupplier.java
+++ src/main/java/dev/scx/io/supplier/ByteInputByteSupplier.java
@@ -25,7 +25,7 @@ public final class ByteInputByteSupplier implements ByteSupplier {
     public ByteChunk get() throws ScxInputException {
         try {
             // 这里我们直接引用 原始 byteInput 中的 ByteChunk, 避免了数组的多次拷贝
-            byteInput.read(consumer, Long.MAX_VALUE);// 我们只尝试拉取一次
+            byteInput.read(consumer, Long.MAX_VALUE); // ByteChunkByteConsumer 保证了只会尝试拉取一次
             return consumer.byteChunk();
         } catch (NoMoreDataException e) {
             // 如果底层 ByteInput 没数据了, 也返回 null
