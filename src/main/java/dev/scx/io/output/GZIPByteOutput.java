04968cb7092d2a981b019b83fd8b076430f3faac
scx567888 <scx567888@outlook.com>
2026-01-28 18:11:43 +0800

    添加 GZIPByteOutput

---
+++ src/main/java/dev/scx/io/output/GZIPByteOutput.java
@@ -0,0 +1,180 @@
+package dev.scx.io.output;
+
+import dev.scx.io.ByteChunk;
+import dev.scx.io.ByteOutput;
+import dev.scx.io.exception.OutputAlreadyClosedException;
+import dev.scx.io.exception.ScxOutputException;
+
+import java.util.zip.CRC32;
+import java.util.zip.Deflater;
+
+/// GZIPByteOutput
+///
+/// @author scx567888
+/// @version 0.0.1
+public final class GZIPByteOutput extends AbstractByteOutput {
+
+    private static final int GZIP_MAGIC = 0x8b1f;
+
+    private final ByteOutput out;
+    private final Deflater def;
+    private final byte[] buffer;
+    private final boolean syncFlush;
+    private final CRC32 crc;
+
+    private boolean headerWritten;
+
+    public GZIPByteOutput(ByteOutput out) {
+        this(out, 1024, false);
+    }
+
+    public GZIPByteOutput(ByteOutput out, int bufferLength) {
+        this(out, bufferLength, false);
+    }
+
+    public GZIPByteOutput(ByteOutput out, boolean syncFlush) {
+        this(out, 1024, syncFlush);
+    }
+
+    public GZIPByteOutput(ByteOutput out, int bufferLength, boolean syncFlush) {
+        if (bufferLength <= 0) {
+            throw new IllegalArgumentException("bufferLength must be greater than 0");
+        }
+        this.out = out;
+        this.def = new Deflater(Deflater.DEFAULT_COMPRESSION, true);
+        this.buffer = new byte[bufferLength];
+        this.syncFlush = syncFlush;
+        this.crc = new CRC32();
+        this.headerWritten = false;
+        this.closed = false;
+    }
+
+    @Override
+    public void write(byte b) throws ScxOutputException, OutputAlreadyClosedException {
+        write(ByteChunk.of(new byte[]{b}));
+    }
+
+    /// 逻辑参考 [java.util.zip.GZIPOutputStream#write(byte\[\], int, int)]
+    @Override
+    public void write(ByteChunk byteChunk) throws ScxOutputException, OutputAlreadyClosedException {
+        ensureOpen();
+
+        ensureHeader();
+
+        ensureNotFinished();
+
+        if (byteChunk.length == 0) {
+            return;
+        }
+
+        // 设置输入
+        def.setInput(byteChunk.bytes, byteChunk.start, byteChunk.length);
+
+        // 如果能够写出就尝试写出.
+        while (!def.needsInput()) {
+            int len = def.deflate(buffer, 0, buffer.length, Deflater.NO_FLUSH);
+            if (len > 0) {
+                out.write(ByteChunk.of(buffer, 0, len));
+            }
+        }
+
+        // crc 校验
+        crc.update(byteChunk.bytes, byteChunk.start, byteChunk.length);
+    }
+
+    /// 逻辑参考 [java.util.zip.DeflaterOutputStream#flush()]
+    @Override
+    public void flush() throws ScxOutputException, OutputAlreadyClosedException {
+        ensureOpen();
+
+        ensureHeader();
+
+        if (syncFlush && !def.finished()) {
+            while (true) {
+                var len = def.deflate(buffer, 0, buffer.length, Deflater.SYNC_FLUSH);
+                if (len > 0) {
+                    out.write(ByteChunk.of(buffer, 0, len));
+                } else {
+                    break;
+                }
+            }
+        }
+
+        out.flush();
+    }
+
+    /// 逻辑参考 [java.util.zip.DeflaterOutputStream#close()]
+    @Override
+    public void close() throws ScxOutputException, OutputAlreadyClosedException {
+        ensureOpen();
+
+        try (def) {
+
+            ensureHeader();
+
+            def.finish();
+
+            while (!def.finished()) {
+                int len = def.deflate(buffer, 0, buffer.length, Deflater.NO_FLUSH);
+                if (len > 0) {
+                    out.write(ByteChunk.of(buffer, 0, len));
+                }
+            }
+
+            out.write(createTrailer());
+            out.close();
+
+        }
+
+        closed = true;
+
+    }
+
+    /// Writes GZIP member header.
+    private void ensureHeader() throws ScxOutputException, OutputAlreadyClosedException {
+        if (headerWritten) {
+            return;
+        }
+        out.write(createHeader());
+        headerWritten = true;
+    }
+
+    private void ensureNotFinished() {
+        if (def.finished()) {
+            throw new ScxOutputException("write beyond end of stream");
+        }
+    }
+
+    private byte[] createHeader() {
+        return new byte[]{
+            (byte) GZIP_MAGIC,         // Magic number (short)
+            (byte) (GZIP_MAGIC >> 8),  // Magic number (short)
+            Deflater.DEFLATED,         // Compression method (CM)
+            0,                         // Flags (FLG)
+            0,                         // Modification time MTIME (int)
+            0,                         // Modification time MTIME (int)
+            0,                         // Modification time MTIME (int)
+            0,                         // Modification time MTIME (int)
+            0,                         // Extra flags (XFLG)
+            (byte) 255                 // Operating system (OS) "OS_UNKNOWN"
+        };
+    }
+
+    private byte[] createTrailer() {
+        // CRC-32 of uncompr. data
+        int crc32 = (int) crc.getValue();
+        // RFC 1952: Size of the original (uncompressed) input data modulo 2^32
+        int iSize = (int) def.getBytesRead();
+        return new byte[]{
+            (byte) (crc32),
+            (byte) (crc32 >>> 8),
+            (byte) (crc32 >>> 16),
+            (byte) (crc32 >>> 24),
+            (byte) (iSize),
+            (byte) (iSize >>> 8),
+            (byte) (iSize >>> 16),
+            (byte) (iSize >>> 24),
+        };
+    }
+
+}
--- src/test/java/dev/scx/io/test/GzipByteOutputTest.java
+++ src/test/java/dev/scx/io/test/GzipByteOutputTest.java
@@ -1,33 +1,74 @@
 package dev.scx.io.test;
 
-import dev.scx.io.ByteChunk;
 import dev.scx.io.ScxIO;
 import dev.scx.io.exception.OutputAlreadyClosedException;
 import dev.scx.io.exception.ScxOutputException;
-import dev.scx.io.output.OutputStreamByteOutput;
+import dev.scx.io.output.EagerByteArrayByteOutput;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.zip.GZIPOutputStream;
 
 public class GzipByteOutputTest {
 
     public static void main(String[] args) throws IOException, OutputAlreadyClosedException, ScxOutputException {
         test1();
+        test2();
     }
 
     @Test
     public static void test1() throws IOException, OutputAlreadyClosedException, ScxOutputException {
-        var bao = new ByteArrayOutputStream();
-        var gzipByteOutput = new OutputStreamByteOutput(bao);
+        var bao = new EagerByteArrayByteOutput();
+        var byteOutput = ScxIO.gzipByteOutput(bao);
 
-        var byteOutput = ScxIO.gzipByteOutput(gzipByteOutput);
+        // 测试 空块写入
+        byteOutput.write(new byte[0]);
 
-        byteOutput.write(ByteChunk.of("abcd".getBytes()));
+        for (int i = 0; i < 1000; i++) {
+            byteOutput.write(("abcd" + i).getBytes());
+        }
 
-        Assert.assertEquals(bao.toByteArray(), new byte[]{31, -117, 8, 0, 0, 0, 0, 0, 0, -1});
+        // 测试 空块写入
+        byteOutput.write(new byte[0]);
 
+        byteOutput.close();
+
+        // jdk 的写入方式
+        var ba = new ByteArrayOutputStream();
+        var gba = new GZIPOutputStream(ba);
+        // 测试 空块写入
+        gba.write(new byte[0]);
+
+        for (int i = 0; i < 1000; i++) {
+            gba.write(("abcd" + i).getBytes());
+        }
+        // 测试 空块写入
+        gba.write(new byte[0]);
+
+        gba.close();
+
+        // 测试 2者 结果是否相等
+        Assert.assertEquals(bao.bytes(), ba.toByteArray());
+    }
+
+    // 测试空流
+    @Test
+    public static void test2() throws IOException, OutputAlreadyClosedException, ScxOutputException {
+        var bao = new EagerByteArrayByteOutput();
+        var byteOutput = ScxIO.gzipByteOutput(bao);
+
+        byteOutput.close();
+
+        // jdk 的写入方式
+        var ba = new ByteArrayOutputStream();
+        var gba = new GZIPOutputStream(ba);
+
+        gba.close();
+
+        // 测试 2者 结果是否相等
+        Assert.assertEquals(bao.bytes(), ba.toByteArray());
     }
 
 }
