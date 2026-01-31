4c38e2319b13bdebfa5c2d7b08d161b5241d6b50
scx567888 <scx567888@outlook.com>
2026-01-31 14:55:54 +0800

    dev

--- src/main/java/dev/scx/io/ByteChunk.java
+++ src/main/java/dev/scx/io/ByteChunk.java
@@ -5,7 +5,6 @@ package dev.scx.io;
 /// 表示 byte[] 上的一个区间视图 [start, end).
 ///
 /// - ByteChunk 本身不拥有数据, 多个 ByteChunk 可能共享同一个 backing byte[].
-/// - 所有索引参数 (如 getByte / toString / subChunk 中的索引) 均为 "相对索引", 即相对于当前 ByteChunk 的起点 (逻辑索引 0).
 /// - ByteChunk 支持通过 subChunk 在当前视图基础上继续创建子视图, 不会发生数据拷贝.
 ///
 /// 为了性能考虑, 本类不做任何边界检查, 调用者需保证参数合法.
@@ -36,32 +35,9 @@ public final class ByteChunk {
         return new ByteChunk(bytes, start, end);
     }
 
+    /// 索引参数 为 "相对索引", 即相对于当前 ByteChunk 的起点 (逻辑索引 0).
     public byte getByte(int index) {
         return this.bytes[this.start + index];
-    }
-
-    public byte[] getBytes(int start, int end) {
-        if (start == 0 && end == this.length && this.start == 0 && this.length == this.bytes.length) {
-            return this.bytes;
-        }
-        var data = new byte[end - start];
-        System.arraycopy(this.bytes, this.start + start, data, 0, data.length);
-        return data;
-    }
-
-    public byte[] getBytes() {
-        return getBytes(0, this.length);
-    }
-
-    /// 默认使用平台编码, 仅建议用于调试
-    public String toString(int start, int end) {
-        return new String(this.bytes, this.start + start, end - start);
-    }
-
-    /// 默认使用平台编码, 仅建议用于调试
-    @Override
-    public String toString() {
-        return toString(0, length);
     }
 
     public ByteChunk subChunk(int start, int end) {
@@ -69,6 +45,12 @@ public final class ByteChunk {
             return this;
         }
         return new ByteChunk(this.bytes, this.start + start, this.start + end);
+    }
+
+    /// 默认使用平台编码, 仅建议用于调试
+    @Override
+    public String toString() {
+        return new String(this.bytes, this.start , this.length);
     }
 
 }
